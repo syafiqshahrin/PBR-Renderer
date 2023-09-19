08b6db03b3699f885dc10a71e3745bfabd51d8ad
Muhammad Syafiq Shahrin <syafiq.shahrin96@gmail.com>
2023-09-19 11:25:18 +0800

    Complete vector header

--- GRW/GRW_Engine/Vector.h
+++ GRW/GRW_Engine/Vector.h
@@ -40,14 +40,7 @@ struct Vector3
 		result.z = z * f;
 		return result;
 	}
-	Vector3 operator*(float const& f) const
-	{
-		Vector3 result;
-		result.x = x * f;
-		result.y = y * f;
-		result.z = z * f;
-		return result;
-	}
+
 	Vector3 operator*(Vector3 const& v2) const
 	{
 		Vector3 result;
@@ -132,10 +125,212 @@ struct Vector3
 
 struct Vector2
 {
+	Vector2() { x = 0, y = 0; }
+	Vector2(float xIn, float yIn) : x(xIn), y(yIn) {}
+	Vector2(Vector2 const& v)
+	{
+		x = v.x;
+		y = v.y;
+	}
+	~Vector2() {}
+	float x, y;
 
+	//operator overloads
+	Vector2 operator+(Vector2 const& v2) const
+	{
+		Vector2 result;
+		result.x = x + v2.x;
+		result.y = y + v2.y;
+		return result;
+	}
+	Vector2 operator-(Vector2 const& v2) const
+	{
+		Vector2 result;
+		result.x = x - v2.x;
+		result.y = y - v2.y;
+		return result;
+	}
+	Vector2 operator*(int const& f) const
+	{
+		Vector2 result;
+		result.x = x * f;
+		result.y = y * f;
+		return result;
+	}
+	Vector2 operator*(Vector2 const& v2) const
+	{
+		Vector2 result;
+		result.x = x * v2.x;
+		result.y = y * v2.y;
+		return result;
+	}
+	Vector2 operator/(float const& f) const
+	{
+		Vector2 result;
+		result.x = x / f;
+		result.y = y / f;
+		return result;
+	}
+	void operator=(Vector2 const& v2)
+	{
+		this->x = v2.x;
+		this->y = v2.y;
+	}
+	//common vector operations
+	//dot
+	float Dot(Vector2 const& v2) const
+	{
+		return (x * v2.x) + (y * v2.y);
+	}
+	float mag() const
+	{
+		return sqrt(x * x + y * y);
+	}
+	//sqrmagnitude 
+	float sqrtmag() const
+	{
+		return x * x + y * y;
+	}
+	//normalize
+	Vector2 GetNormalize() const
+	{
+		float m = mag();
+		return *this / m;
+	}
+	void Normalize()
+	{
+		float m = mag();
+		*this = *this / m;
+	}
+
+	std::string prnt() const
+	{
+		std::string s = std::to_string(x) + ", " + std::to_string(y);
+		return s;
+	}
+	//distance
+	float Dist(Vector2 const& v2) const
+	{
+		Vector2 d = *this - v2;
+		return d.mag();
+	}
+	//cosine angle in radians
+	float Angle(Vector2 const& v2) const
+	{
+		float d = this->Dot(v2);
+		float aM = this->mag();
+		float bM = v2.mag();
+		float angle = acos(d / (aM * bM));
+		return angle;
+	}
 };
 
 struct Vector4
 {
+	Vector4() { x = 0, y = 0, z = 0, w = 0; }
+	Vector4(float xIn, float yIn, float zIn, float wIn) : x(xIn), y(yIn), z(zIn) , w(wIn) {}
+	Vector4(Vector4 const& v)
+	{
+		x = v.x;
+		y = v.y;
+		z = v.z;
+		w = v.w;
+	}
+	~Vector4() {}
+	float x, y, z, w;
 
+	//operator overloads
+	Vector4 operator+(Vector4 const& v2) const
+	{
+		Vector4 result;
+		result.x = x + v2.x;
+		result.y = y + v2.y;
+		result.z = z + v2.z;
+		result.w = w + v2.w;
+		return result;
+	}
+	Vector4 operator-(Vector4 const& v2) const
+	{
+		Vector4 result;
+		result.x = x - v2.x;
+		result.y = y - v2.y;
+		result.z = z - v2.z;
+		result.w = w - v2.w;
+		return result;
+	}
+	Vector4 operator*(int const& f) const
+	{
+		Vector4 result;
+		result.x = x * f;
+		result.y = y * f;
+		result.z = z * f;
+		result.w = w * f;
+		return result;
+	}
+
+	Vector4 operator*(Vector4 const& v2) const
+	{
+		Vector4 result;
+		result.x = x * v2.x;
+		result.y = y * v2.y;
+		result.z = z * v2.z;
+		result.w = w * v2.w;
+		return result;
+	}
+	Vector4 operator/(float const& f) const
+	{
+		Vector4 result;
+		result.x = x / f;
+		result.y = y / f;
+		result.z = z / f;
+		result.w = w / f;
+		return result;
+	}
+	void operator=(Vector4 const& v2)
+	{
+		this->x = v2.x;
+		this->y = v2.y;
+		this->z = v2.z;
+		this->w = v2.w;
+	}
+	//common vector operations
+	//dot
+	float Dot(Vector4 const& v2) const
+	{
+		return (x * v2.x) + (y * v2.y) + (z * v2.z) + (w * v2.w);
+	}
+
+	//magnitude
+	float mag() const
+	{
+		return sqrt(x * x + y * y + z * z + w * w);
+	}
+	//sqrmagnitude 
+	float sqrtmag() const
+	{
+		return x * x + y * y + z * z + w * w;
+	}
+	//normalize
+	Vector4 GetNormalize() const
+	{
+		float m = mag();
+		return *this / m;
+	}
+	void Normalize()
+	{
+		float m = mag();
+		*this = *this / m;
+	}
+
+	std::string prnt() const
+	{
+		std::string s = std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ", " + std::to_string(w);
+		return s;
+	}
+	//distance
+	float Dist(Vector4 const& v2) const
+	{
+		Vector4 d = *this - v2;
+		return d.mag();
+	}
 };
