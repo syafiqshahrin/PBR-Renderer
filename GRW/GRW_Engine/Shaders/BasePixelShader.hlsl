e3d81296e9a01ff08e628b259eb076e46fd1568f
Syafiq Shahrin <syafiq.shahrin96@gmail.com>
2023-09-26 23:22:34 +0800

    tests

--- GRW/GRW_Engine/Shaders/BasePixelShader.hlsl
+++ GRW/GRW_Engine/Shaders/BasePixelShader.hlsl
@@ -17,7 +17,8 @@ float4 main(VSOutput pIN) : SV_TARGET
 {
 	float timeScaled = time.x * 0.01;
 
-	float4 color = lerp(float4(pIN.norm, 1), float4(sin(timeScaled + pIN.col.x) + 1 * 0.5, sin(timeScaled + pIN.col.y) + 1 * 0.5, sin(timeScaled + pIN.col.z) + 1 * 0.5, 1), sin(timeScaled + pIN.col.x) + 1 * 0.5);
+	//float4 color = lerp(float4(pIN.norm, 1), float4(sin(timeScaled + pIN.col.x) + 1 * 0.5, sin(timeScaled + pIN.col.y) + 1 * 0.5, sin(timeScaled + pIN.col.z) + 1 * 0.5, 1), 1);
+	float4 color = float4(pIN.norm, 0);
 	return color;
 	//return float4(1,0,0,1);
 }
